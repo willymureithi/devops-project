99f1dbc741fe9274c8f83ae2de3ac7fa4af3f63d
willymureithi <mureithiwilson95@gmail.com>
2024-03-06 14:57:16 +0300

    update terraform with vpc and foreach

--- terraform_code/V1-EC1.tf
+++ terraform_code/V1-EC1.tf
@@ -3,14 +3,23 @@ provider "aws" {
 }
 
 resource "aws_instance" "demo-server" {
-    ami = "ami-0f403e3180720dd7e"
+    ami = "ami-07d9b9ddc6cd8dd30"
     instance_type = "t2.micro"
     key_name = "dpp"
+    //security_groups = [ "demo-sg" ]
+    vpc_security_group_ids = [ aws_security_group.demo-sg.id]
+    subnet_id = aws_subnet.dpp-public-subnet-01.id
+    for_each = toset(["jenkins-master", "build-slave", "ansible"])
+   tags = {
+     Name = "${each.key}"
+   }
   
 }
+
 resource "aws_security_group" "demo-sg" {
   name        = "demo-sg"
   description = "SSH Access"
+  vpc_id = aws_vpc.dpp-vpc.id
   
   ingress {
     description      = "Shh access"
@@ -32,4 +41,57 @@ resource "aws_security_group" "demo-sg" {
     Name = "ssh-port"
 
   }
+}
+
+resource "aws_vpc" "dpp-vpc" {
+  cidr_block = "10.1.0.0/16"
+  tags = {
+    Name = "dpp-vpc"
+  }
+  
+}
+
+resource "aws_subnet" "dpp-public-subnet-01" {
+  vpc_id = aws_vpc.dpp-vpc.id
+  cidr_block = "10.1.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "dpp-public-subent-01"
+  }
+}
+
+resource "aws_subnet" "dpp-public-subnet-02" {
+  vpc_id = aws_vpc.dpp-vpc.id
+  cidr_block = "10.1.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "dpp-public-subent-02"
+  }
+}
+
+resource "aws_internet_gateway" "dpp-igw" {
+  vpc_id = aws_vpc.dpp-vpc.id 
+  tags = {
+    Name = "dpp-igw"
+  } 
+}
+
+resource "aws_route_table" "dpp-public-rt" {
+  vpc_id = aws_vpc.dpp-vpc.id 
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.dpp-igw.id 
+  }
+}
+
+resource "aws_route_table_association" "dpp-rta-public-subnet-01" {
+  subnet_id = aws_subnet.dpp-public-subnet-01.id
+  route_table_id = aws_route_table.dpp-public-rt.id   
+}
+
+resource "aws_route_table_association" "dpp-rta-public-subnet-02" {
+  subnet_id = aws_subnet.dpp-public-subnet-02.id 
+  route_table_id = aws_route_table.dpp-public-rt.id   
 }
